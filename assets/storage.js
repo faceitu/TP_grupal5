f91b16926fe05b8ae2bab6c5c91c05249dd65191
Fernando Aceituno <aceituno.fernando@gmail.com>
2022-10-05 13:28:53 -0300

    menu navbar y header2

---
+++ assets/storage.js
@@ -0,0 +1,17 @@
+const categorias = ['Pizzas', 'Hamburguesas', 'Napapuki', 'individuales', 'Tronco-Pizza', 'Papas / Provensal']
+const menu = [{
+        id: 1,
+        name: 'La Mr. Pit',
+        precio: '$350',
+        data: 'solo para expertos',
+        img: '"./assets/img/menu0.png"'
+
+    },
+    {
+        id: 2,
+        name: '¡Q Jammone!',
+        precio: '$350',
+        data: 'c / jamon crudo ',
+        img: '"./assets/img/menu1.png"'
+    }
+]
