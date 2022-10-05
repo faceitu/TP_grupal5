f2b6f65db2c2f82b154fedc2c8d875993bf5cb80
Fernando Aceituno <aceituno.fernando@gmail.com>
2022-10-05 19:28:26 -0300

    con los arrays

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,32 @@
-console.log(categorias)
+const reco = document.getElementById('recom')
+
+
+
+const rendersection = menu => {
+
+    return `<div class = "card" >
+        <img src = "${menu.img}" >
+        <div class = "text_card" >
+            <span class = "tittle_card" > Bennaziana </span> 
+            <p class = "data_card" > La mas completa</p> 
+            <span > $ 3650 </span> 
+            </div> 
+            <div class = "btn_card" > Agregar </div> 
+        </div> `
+};
+
+const renderPage = () => {
+    reco.innerHTML = recomenda.map(prod => rendersection(prod)).join('')
+
+}
+
+
+
+
+const init = () => {
+    window.addEventListener('DOMContentLoaded', renderPage);
+
+}
+
+
+init()
--- assets/storage.js
+++ assets/storage.js
@@ -3,14 +3,14 @@ const recomenda = [{
         name: 'Bennazianna',
         precio: '$3650',
         data: 'La mas completa',
-        img: '"./assets/img/menu0.png"'
+        img: './assets/img/menu_reco1.png'
     },
     {
         id: 2,
         name: 'Tronco-Pizza',
         precio: '$870',
         data: 'Para todo el dia',
-        img: '"./assets/img/menu0.png"'
+        img: './assets/img/menu_reco2.png'
 
     },
     {
@@ -18,66 +18,69 @@ const recomenda = [{
         name: 'Papas / Provenzal',
         precio: '$360',
         data: 'Van como piña',
-        img: '"./assets/img/menu0.png"'
+        img: './assets/img/menu_reco3.png'
 
     },
 ]
 const categorias = ['Pizzas', 'Hamburguesas', 'Napapuki', 'individuales', 'Tronco-Pizza', 'Papas / Provensal']
-const menu = [{
-        id: 1,
-        name: 'La Mr. Pit',
-        precio: '$350',
-        data: 'solo para expertos',
-        img: '"./assets/img/menu0.png"'
 
-    },
-    {
-        id: 2,
-        name: '¡Q Jammone!',
-        precio: '$350',
-        data: 'c / jamon crudo ',
-        img: '"./assets/img/menu1.png"'
-    },
-    {
-        id: 3,
-        name: 'La Charly Garcia',
-        precio: '$380',
-        data: '¡Basta!',
-        img: '"./assets/img/menu2.png"'
-    },
-    {
-        id: 4,
-        name: 'La Maradona',
-        precio: '$450',
-        data: '¡Eterna!',
-        img: '"./assets/img/menu4.png"'
-    },
-    {
-        id: 5,
-        name: 'Picantovich',
-        precio: '$750',
-        data: 'Pica 2 veces',
-        img: '"./assets/img/menu5.png"'
-    },
-    {
-        id: 6,
-        name: 'La Hasbulla',
-        precio: '$990',
-        data: 'En honor al 1',
-        img: '"./assets/img/menu6.png"'
-    },
-    {
-        id: 7,
-        name: 'Leo Messi',
-        precio: '$10',
-        data: '¡De pie señores!',
-        img: '"./assets/img/menu7.png"'
-    },
-    {
-        id: 8,
-        name: 'Nick Gi',
-        precio: '$Gratis',
-        data: 'La que desaparece',
-        img: '"./assets/img/menu4.png"'
-    },
-]
+const menu =
+
+    [{
+            id: 1,
+            name: 'La Mr. Pit',
+            precio: '$350',
+            data: 'solo para expertos',
+            img: '"./assets/img/menu0.png"'
+
+        },
+        {
+            id: 2,
+            name: '¡Q Jammone!',
+            precio: '$350',
+            data: 'c / jamon crudo ',
+            img: '"./assets/img/menu1.png"'
+        },
+        {
+            id: 3,
+            name: 'La Charly Garcia',
+            precio: '$380',
+            data: '¡Basta!',
+            img: '"./assets/img/menu2.png"'
+        },
+        {
+            id: 4,
+            name: 'La Maradona',
+            precio: '$450',
+            data: '¡Eterna!',
+            img: '"./assets/img/menu4.png"'
+        },
+        {
+            id: 5,
+            name: 'Picantovich',
+            precio: '$750',
+            data: 'Pica 2 veces',
+            img: '"./assets/img/menu5.png"'
+        },
+        {
+            id: 6,
+            name: 'La Hasbulla',
+            precio: '$990',
+            data: 'En honor al 1',
+            img: '"./assets/img/menu6.png"'
+        },
+        {
+            id: 7,
+            name: 'Leo Messi',
+            precio: '$10',
+            data: '¡De pie señores!',
+            img: '"./assets/img/menu7.png"'
+        },
+        {
+            id: 8,
+            name: 'Nick Gi',
+            precio: '$Gratis',
+            data: 'La que desaparece',
+            img: '"./assets/img/menu4.png"'
+        },
+    ]
