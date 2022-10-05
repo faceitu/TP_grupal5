1c06495dbaa618dcfbc562fb08fa4b4b924be2f7
Fernando Aceituno <aceituno.fernando@gmail.com>
2022-10-05 15:04:32 -0300

    con los arrays

--- assets/storage.js
+++ assets/storage.js
@@ -13,5 +13,47 @@ const menu = [{
         precio: '$350',
         data: 'c / jamon crudo ',
         img: '"./assets/img/menu1.png"'
-    }
+    },
+    {
+        id: 3,
+        name: 'La Charly Garcia',
+        precio: '$380',
+        data: '¡Basta!',
+        img: '"./assets/img/menu2.png"'
+    },
+    {
+        id: 4,
+        name: 'La Maradona',
+        precio: '$450',
+        data: '¡Eterna!',
+        img: '"./assets/img/menu4.png"'
+    },
+    {
+        id: 5,
+        name: 'Picantovich',
+        precio: '$750',
+        data: 'Pica 2 veces',
+        img: '"./assets/img/menu5.png"'
+    },
+    {
+        id: 6,
+        name: 'La Hasbulla',
+        precio: '$990',
+        data: 'En honor al 1',
+        img: '"./assets/img/menu6.png"'
+    },
+    {
+        id: 7,
+        name: 'Leo Messi',
+        precio: '$10',
+        data: '¡De pie señores!',
+        img: '"./assets/img/menu7.png"'
+    },
+    {
+        id: 8,
+        name: 'Nick Gi',
+        precio: '$Gratis',
+        data: 'La que desaparece',
+        img: '"./assets/img/menu4.png"'
+    },
 ]
